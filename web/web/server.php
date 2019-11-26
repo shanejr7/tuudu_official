9ffcdebfa338e5deaa09e04940ee8ff090e229c6
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2019-11-26 11:15:53 -0500

    v1.0

--- web/web/server.php
+++ web/web/server.php
@@ -276,7 +276,7 @@ $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!'
 <?php
 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
     // FIXME: add more validation, e.g. using ext/fileinfo
-     $destination = "/organization_event_file/". $_FILES['userfile']['name'];
+     $destination = "organization_event_file/". $_FILES['userfile']['name'];
     try {
         // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
         $upload = $s3->upload($bucket, $destination, fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
