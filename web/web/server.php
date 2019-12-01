5f2e30c425a2f1705b1447d016b38023a449a30c
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2019-12-01 18:11:34 -0500

    require Mailgun.php

--- web/web/server.php
+++ web/web/server.php
@@ -211,11 +211,12 @@ if (isset($_POST['login_user'])) {
  
 require '../../vendor/autoload.php';
  
-include '../../vendor/mailgun/mailgun-php/src/Mailgun.php';
+// include '../../vendor/mailgun/mailgun-php/src/Mailgun.php';
+use Mailgun\Mailgun;
  
  
-$mgClient = new Mailgun('3c3cf6e0e1734cfbcd9fbf8f1fd6d011-e470a504-8d00075c');
-
+ 
+$mgClient = Mailgun::create('3c3cf6e0e1734cfbcd9fbf8f1fd6d011-e470a504-8d00075c'); // For US servers
 
 $domain = "sandboxfa5d66d41cd74a59bd70dc47dc88118e.mailgun.org";
  
