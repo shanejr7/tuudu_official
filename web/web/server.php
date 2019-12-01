a32c101f8e67740078f6f54abfd413f771458ad0
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2019-11-30 20:47:49 -0500

    sendgrid

--- web/web/server.php
+++ web/web/server.php
@@ -211,28 +211,24 @@ if (isset($_POST['login_user'])) {
  
 require '../../vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
+// Comment out the above line if not using Composer
 // require("<PATH TO>/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("aeravi.io", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("shane.jr7@icloud.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
 try {
-    $response = $sendgrid->send($email);
+    $response = $sg->client->suppression()->bounces()->get();
     print $response->statusCode() . "\n";
     print_r($response->headers());
     print $response->body() . "\n";
 } catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    echo 'Caught exception: '.  $e->getMessage(). "\n";
 }
  
 
