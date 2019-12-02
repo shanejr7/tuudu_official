a24eb519113259b9feb5babac050cc61da883192
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2019-12-02 15:08:55 -0500

    mailgun added

--- web/web/server.php
+++ web/web/server.php
@@ -220,7 +220,7 @@ if (isset($_POST['login_user'])) {
  
 $mgClient = Mailgun::create('3c3cf6e0e1734cfbcd9fbf8f1fd6d011-e470a504-8d00075c'); // For US servers
 
-$domain = "sandboxfa5d66d41cd74a59bd70dc47dc88118e.mailgun.org";
+$domain = "https://api.mailgun.net/v3/sandboxfa5d66d41cd74a59bd70dc47dc88118e.mailgun.org";
  
 $result = $mgClient->messages()->send($domain, [
 'from' => 'smj.johnsonjr@gmail.com',
