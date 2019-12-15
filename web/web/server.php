b7d5e6c1c33beabef275242468844f47bfd56a1b
SJR-2 <32821770+SJR-2@users.noreply.github.com>
2019-12-14 21:10:59 -0500

    email template

--- web/web/server.php
+++ web/web/server.php
@@ -597,7 +597,7 @@ header('location: profile.php');
 <div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:0px; padding-bottom:0px; padding-right: 0px; padding-left: 0px;">
 <!--<![endif]-->
 <div align="center" class="img-container center fixedwidth" style="padding-right: 0px;padding-left: 0px;">
-<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]--><img align="center" alt="CEO" border="0" class="center fixedwidth" src="images/logo_size.jpg" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 186px; display: block;" title="CEO" width="186"/>
+<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]--><img align="center" alt="CEO" border="0" class="center fixedwidth" src="../assets/templates/images/logo_size.jpg" style="text-decoration: none; -ms-interpolation-mode: bicubic; border: 0; height: auto; width: 100%; max-width: 186px; display: block;" title="CEO" width="186"/>
 <!--[if mso]></td></tr></table><![endif]-->
 </div>
 <!--[if (!mso)&(!IE)]><!-->
