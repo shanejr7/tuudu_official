f748d6eb53475f6598d28df6385dcdb2fa411e2a
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-10 21:15:36 -0400

    update

--- web/web/profile.php
+++ web/web/profile.php
@@ -373,7 +373,7 @@ if (isset($temp) && $temp ==1) {
     
         
         <div class="row">
-          <div class="col-md-6 ml-auto mr-auto">
+          <div class="col-md-12 ml-auto mr-auto">
             <div class="profile-tabs">
               <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
                 <li class="nav-item">
