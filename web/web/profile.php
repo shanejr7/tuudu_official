f9935fe5ef63ebee3a717f78d2a32a18ffdcdb89
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-15 12:05:43 -0400

    margin

--- web/web/profile.php
+++ web/web/profile.php
@@ -857,7 +857,7 @@ pg_close($db);
               $presignedUrl = (string)$request->getUri();
 
 
-              echo ' <div class="col-md-3 ml-auto card card-product" style="margin-right:1px;" >
+              echo ' <div class="col-md-3 ml-auto card card-product"  >
                  <div style="padding-right: 2em;">';
 
           if($presignedUrl){
