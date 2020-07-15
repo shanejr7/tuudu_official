e06ba5f1766f31704edeee3200eb8b5dd0fcd67d
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-15 18:27:42 -0400

    update profile image uploader

--- web/web/profile.php
+++ web/web/profile.php
@@ -335,11 +335,11 @@ $bucket_name = 'tuudu-official-file-storage';
 
         </div>
 
-          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="uploadImage" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Terms & Agreement</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Profile Picture</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
