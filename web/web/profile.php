1da906476dd3040e2fd8586d0bc310ed3eb60408
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-17 13:27:27 -0400

    stats php home tab

--- web/web/profile.php
+++ web/web/profile.php
@@ -525,7 +525,7 @@ catch (S3Exception $e) {
 
                         $collections_num_count = sizeof($collections_count) + sizeof($product_count) + sizeof($tag_schedule_count) + sizeof($user_follow_count);
 
-                        echo '<li style="display: inline-block;"><b>'.$user_follow_organization_count.'</b> Collections</li>';
+                        echo '<li style="display: inline-block;"><b>'.$collections_num_count.'</b> Collections</li>';
                         
                       }
 
