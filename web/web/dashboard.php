24549978f8bce9dc6f5b5ba5d1558509cfcecbaf
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-20 12:23:15 -0400

    update

--- web/web/dashboard.php
+++ web/web/dashboard.php
@@ -894,12 +894,7 @@ if (isset($stories_list)) {
 
  $("a").on('click', function() {
     history.replaceState(null, null, ' ');
-     $('#tabTrack a').each(function() {
-      $(this).removeClass('active');
-    });
-    $('#body div').each(function() {
-      $(this).removeClass('active');
-    });
+  
      $('#tabTrack li').each(function() {
       $(this).removeClass('active');
     });
