be19797dfb38471837d373f8eb59800d93cd12d3
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-22 15:17:48 -0400

    update chat design AJAX

--- web/web/chat/fetch_user_post.php
+++ web/web/chat/fetch_user_post.php
@@ -5,6 +5,7 @@
 	if (isset($_POST['id']) && isset($_POST['publickey'])) {
 
 		$publickey = pg_escape_string($db, $_POST['publickey']);
+		$publickey = trim($publickey);
 		$user_id = pg_escape_string($db, $_POST['id']);
 		
 			$db="";
@@ -21,7 +22,8 @@
 
 
 
-				$result = pg_query($db, "SELECT C.id as post_id, C.publickey as post_publickey ,C.email as post_email, C.description as post_description, C.date_submitted as post_submitted,Z.id as user_id, Z.email as user_email, Z.publickey as user_publickey,Z.username as user_username FROM organization C ,users Z  WHERE C.id = $user_id  AND C.publickey ='$publickey'  LIMIT 1");
+				$result = pg_query($db, "SELECT C.id as post_id, C.publickey as post_publickey ,C.email as post_email, C.description as post_description, C.date_submitted as post_submitted,Z.id as user_id, Z.email as user_email, Z.publickey as user_publickey,Z.username as user_username FROM organization C ,users Z WHERE C.id = $user_id  AND Z.id =$user_id AND C.publickey ='$publickey'");
+				
   				
   				$user_post = pg_fetch_assoc($result);
   
