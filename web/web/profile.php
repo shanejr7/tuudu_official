c2b2365a9903b1daa71af735bbc440dccb2cb8d3
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-22 10:38:00 -0400

    update chat design

--- web/web/profile.php
+++ web/web/profile.php
@@ -1278,7 +1278,13 @@ pg_close($db);
       </div>
       <div class="section section-comments">
         <div class="row">
-          <div class="col-md-8 ml-auto mr-auto">
+          <div class="col-md-8 ml-auto mr-auto" style="display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+
+  border: 2px dashed rgba(114, 186, 94, 0.35);
+  height: 400px;
+  background: rgba(114, 186, 94, 0.05);">
             <div class="media-area">
      
               <div class="media">
