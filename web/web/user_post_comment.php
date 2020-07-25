5f7bd80037803be4b61a810748769647a8d5f1d4
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-25 19:05:17 -0400

    update chat design AJAX

--- web/web/user_post_comment.php
+++ web/web/user_post_comment.php
@@ -29,12 +29,17 @@ if (isset($_POST['id']) && isset($_POST['publickey']) && isset($_POST['post']) &
  		$userid = filter_var($_POST['id'], FILTER_SANITIZE_STRING);
  		$publickey = filter_var($_POST['publickey'], FILTER_SANITIZE_STRING);
  		$post = filter_var($_POST['post'], FILTER_SANITIZE_STRING);
+ 		$post = trim($post);
  		$username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
  		$username = trim($username);
 
- 		pg_query($db, "INSERT INTO public.messagestate(
+ 		if ($post!= "" || $post!= " ") {
+ 			 	pg_query($db, "INSERT INTO public.messagestate(
 	user_id, message, publickey, reply_to_id, timestamp_message, favorite, username)
 	VALUES ($userid, '$post', '$publickey', $replyid, now(), 0, '$username')");
+ 		}
+
+ 	 
 
  		$data.='<textarea class="form-control" rows="5" value="" id="postText"></textarea>';
 
