23eefefcd14f147b6ae2d106a42078776f4bb66c
Shane Michael Johnson <shane@MacBook-Air.local>
2020-07-30 13:11:04 -0400

    update

--- web/web/profile.php
+++ web/web/profile.php
@@ -1371,12 +1371,16 @@ $(document).ready(function() {
 
 $(document).on('click', '.avatar_uploader_form', function () {
 var file_data = $("#upimg").prop("files")[0]; 
+
+
 var form_data = new FormData();  
 form_data.append("file", file_data);
 var id=$(this).data("userid");
     
 console.log("clicked");
 console.log(form_data + " "+ id);
+
+alert(form_data);
 
   update_avatar(id,form_data);
 
