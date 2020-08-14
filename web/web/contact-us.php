7a692ebdf4abadd3c5b70e5471733e91a7f0084c
Shane Michael Johnson <shane@MacBook-Air.local>
2020-08-14 10:10:55 -0400

    contact-us.php page

--- web/web/contact-us.php
+++ web/web/contact-us.php
@@ -1,9 +1,6 @@
-<?php include('server.php');
- 
-   ?>
 <!--
  =========================================================
- Material Kit PRO - v2.1.1
+ Material Kit PRO - v2.2.0
  =========================================================
 
  Product Page: https://www.creative-tim.com/product/material-kit-pro
@@ -15,67 +12,144 @@
 
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8" />
-    
- <link rel="icon" 
-      type="image/jpg" 
-      href="../assets/img/logo_size.jpg"/>
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    forgot password
+    Material Kit PRO by Creative Tim
   </title>
-
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-   <link href="../assets/css/material-kit.css?v=2.2.0" rel="stylesheet" />
+  <link href="../assets/css/material-kit.css?v=2.2.0" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="../assets/demo/demo.css" rel="stylesheet" />
   <link href="../assets/demo/vertical-nav.css" rel="stylesheet" />
- <script src="../assets/js/local.js"></script>
-
 </head>
 
-<body class="profile-page sidebar-collapse">
-
-<nav class="navbar navbar-expand-lg bg-primary">
+<body class="contact-page sidebar-collapse">
+  <nav class="navbar    fixed-top  navbar-expand-lg bg-dark" color-on-scroll="100" id="sectionsNav">
     <div class="container">
-        <div class="navbar-translate col-lg-4">
-
-            <a class="navbar-brand" href="login-page.php">  <img src="../assets/img/logo.png" style="width: 30%; "></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon"></span>
-            <span class="navbar-toggler-icon"></span>
-            <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
-        <div class="collapse navbar-collapse col-lg-8">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="login-page.php" class="nav-link">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a href="signup-page.php" class="nav-link">Signup</a>
-                </li>
-               
-            </ul>
-    
-          
-        </div>
-    
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit-pro/index.html">
+          Material Kit PRO </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">apps</i> Components
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="../presentation.html" class="dropdown-item">
+                <i class="material-icons">line_style</i> Presentation
+              </a>
+              <a href="../index.html" class="dropdown-item">
+                <i class="material-icons">layers</i> All Components
+              </a>
+              <a href="http://demos.creative-tim.com/material-kit-pro/docs/2.1/getting-started/introduction.html" class="dropdown-item">
+                <i class="material-icons">content_paste</i> Documentation
+              </a>
+            </div>
+          </li>
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">view_day</i> Sections
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="../sections.html#headers" class="dropdown-item">
+                <i class="material-icons">dns</i> Headers
+              </a>
+              <a href="../sections.html#features" class="dropdown-item">
+                <i class="material-icons">build</i> Features
+              </a>
+              <a href="../sections.html#blogs" class="dropdown-item">
+                <i class="material-icons">list</i> Blogs
+              </a>
+              <a href="../sections.html#teams" class="dropdown-item">
+                <i class="material-icons">people</i> Teams
+              </a>
+              <a href="../sections.html#projects" class="dropdown-item">
+                <i class="material-icons">assignment</i> Projects
+              </a>
+              <a href="../sections.html#pricing" class="dropdown-item">
+                <i class="material-icons">monetization_on</i> Pricing
+              </a>
+              <a href="../sections.html#testimonials" class="dropdown-item">
+                <i class="material-icons">chat</i> Testimonials
+              </a>
+              <a href="../sections.html#contactus" class="dropdown-item">
+                <i class="material-icons">call</i> Contacts
+              </a>
+            </div>
+          </li>
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">view_carousel</i> Examples
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="../examples/about-us.html" class="dropdown-item">
+                <i class="material-icons">account_balance</i> About Us
+              </a>
+              <a href="../examples/blog-post.html" class="dropdown-item">
+                <i class="material-icons">art_track</i> Blog Post
+              </a>
+              <a href="../examples/blog-posts.html" class="dropdown-item">
+                <i class="material-icons">view_quilt</i> Blog Posts
+              </a>
+              <a href="../examples/contact-us.html" class="dropdown-item">
+                <i class="material-icons">location_on</i> Contact Us
+              </a>
+              <a href="../examples/landing-page.html" class="dropdown-item">
+                <i class="material-icons">view_day</i> Landing Page
+              </a>
+              <a href="../examples/login-page.html" class="dropdown-item">
+                <i class="material-icons">fingerprint</i> Login Page
+              </a>
+              <a href="../examples/pricing.html" class="dropdown-item">
+                <i class="material-icons">attach_money</i> Pricing Page
+              </a>
+              <a href="../examples/shopping-cart.html" class="dropdown-item">
+                <i class="material-icons">shopping_basket</i> Shopping Cart
+              </a>
+              <a href="../examples/ecommerce.html" class="dropdown-item">
+                <i class="material-icons">store</i> Ecommerce Page
+              </a>
+              <a href="../examples/product-page.html" class="dropdown-item">
+                <i class="material-icons">shopping_cart</i> Product Page
+              </a>
+              <a href="../examples/profile-page.html" class="dropdown-item">
+                <i class="material-icons">account_circle</i> Profile Page
+              </a>
+              <a href="../examples/signup-page.html" class="dropdown-item">
+                <i class="material-icons">person_add</i> Signup Page
+              </a>
+            </div>
+          </li>
+          <li class="button-container nav-item iframe-extern">
+            <a href="https://www.creative-tim.com/product/material-kit-pro?ref=presentation" target="_blank" class="btn  btn-rose   btn-round btn-block">
+              <i class="material-icons">shopping_cart</i> Buy Now
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
- <div id="contactUsMap" class="big-map"></div>
-    <div class="main main-raised">
+  </nav>
+  <div id="contactUsMap" class="big-map"></div>
+  <div class="main main-raised">
     <div class="contact-content">
       <div class="container">
         <h2 class="title">Send us a message</h2>
@@ -149,56 +223,28 @@
       </div>
     </div>
   </div>
-
-    <div class="container">
-        <div class="row">
-       
-   
-
-
-
- 
- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Terms & Agreement</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
- by accepting this agreement and terms with using our services. you agree that all transactions and events are affiliated to independent third-party organizations in which our services only provides data without any association or having liability with third-party organizations. accepting these terms our services is allowed to use data of your liking to provide events and or activitites.If you do not agree with our terms and policy then do not register, download, or use our services.
-      </div>
-      <div class="modal-footer">
-        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Ok</button> -->
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-  <footer class="footer footer-default">
+  <footer class="footer">
     <div class="container">
       <nav class="float-left">
-     <ul>
+        <ul>
           <li>
-            <a href="https://www.Aeravi.io">
-              Aeravi
+            <a href="https://www.creative-tim.com">
+              Creative Tim
             </a>
           </li>
           <li>
-            <a href="https://www.Aeravi.io">
+            <a href="https://creative-tim.com/presentation">
               About Us
             </a>
           </li>
           <li>
-            <a href="https://www.Aeravi.io">
-              Licenses
+            <a href="http://blog.creative-tim.com">
+              Blog
             </a>
           </li>
-           <li>
-           <a href="#" data-toggle="modal" data-target="#exampleModal">
-            Terms
+          <li>
+            <a href="https://www.creative-tim.com/license">
+              Licenses
             </a>
           </li>
         </ul>
@@ -207,29 +253,29 @@
         &copy;
         <script>
           document.write(new Date().getFullYear())
-        </script> created by 
-        <a href="https://www.aeravi.io">Aeravi</a>.
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
       </div>
     </div>
   </footer>
-  </div>
- 
+  <!--   Core JS Files   -->
   <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
   <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
   <script src="../assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
   <script src="../assets/js/plugins/moment.min.js"></script>
-  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <!--  Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
   <script src="../assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
   <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAusWjyb2Li6ezpM7E6kpUsDwXdaMWPHUE"></script>
+  <!--  Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
   <script src="../assets/js/plugins/bootstrap-tagsinput.js"></script>
-  <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+  <!--  Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
   <script src="../assets/js/plugins/bootstrap-selectpicker.js" type="text/javascript"></script>
-  <!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+  <!--  Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
   <script src="../assets/js/plugins/jasny-bootstrap.min.js" type="text/javascript"></script>
-  <!--	Plugin for Small Gallery in Product Page -->
+  <!--  Plugin for Small Gallery in Product Page -->
   <script src="../assets/js/plugins/jquery.flexisel.js" type="text/javascript"></script>
   <!-- Plugins for presentation and navigation  -->
   <script src="../assets/demo/modernizr.js" type="text/javascript"></script>
@@ -239,8 +285,8 @@
   <!-- Js With initialisations For Demo Purpose, Don't Include it in Your Project -->
   <script src="../assets/demo/demo.js" type="text/javascript"></script>
   <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/material-kit.js?v=2.1.1" type="text/javascript"></script>
-   <script>
+  <script src="../assets/js/material-kit.js?v=2.2.0" type="text/javascript"></script>
+  <script>
     $().ready(function() {
       materialKitDemo.initContactUsMap();
     });
