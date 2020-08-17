8602421dfcc6f5c4afa3782fd9c20d6a9269c6ec
Shane Michael Johnson <shane@MacBook-Air.local>
2020-08-17 11:07:40 -0400

    post organization type

--- web/web/post.php
+++ web/web/post.php
@@ -239,7 +239,7 @@ pg_close($db);
             <div class="col-lg-7 mr-auto">
          <div class="card card-login">
           
-      <div class="col-lg-8">
+      <div class="col-lg-10">
                      <!-- <h1 class="title">event submission</h1>
  -->                <div class="main object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="100">
                 <div class="form-block p-30" style="margin-top: 5em; margin-left: 2em;" >
