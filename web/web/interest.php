88190e77d00c6ff5031e0ecf08674f51bfcc5ec9
Shane Michael Johnson <shane@MacBook-Air.local>
2020-08-18 09:36:21 -0400

    update interest

--- web/web/interest.php
+++ web/web/interest.php
@@ -60,7 +60,7 @@ if (!isset($_SESSION['username'])) {
     <div class="container">
         <div class="navbar-translate col-lg-4">
 
-           <!--  <a class="navbar-brand" href="login-page.php">  <img src="../assets/img/logo.png" style="width: 10%; "></a> -->
+            <a class="navbar-brand" href="login-page.php">  <img src="../assets/img/logo.png" style="width: 30%; "></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
             <span class="navbar-toggler-icon"></span>
