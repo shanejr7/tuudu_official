a69fe5706728acb1ff2822ee389e73f27d0c0e13
Shane Michael Johnson <shane@MacBook-Air.local>
2020-09-18 10:55:38 -0400

    update

--- web/web/login-page.php
+++ web/web/login-page.php
@@ -101,7 +101,7 @@
         <div class="collapse navbar-collapse col-lg-8">
               <ul class="navbar-nav">
               <li class="nav-item">
-                    <a href="home.php" class="nav-link">Home</a>
+                    <a href="home.php" class="nav-link"><i class="material-icons">home</i></a>
                 </li>
                 <li class="nav-item active">
                     <a href="login-page.php" class="nav-link">Login</a>
