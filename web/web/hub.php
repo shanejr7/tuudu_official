06a5f217df4a2ad1c0a4600b997e1eafe589820a
Shane Michael Johnson <shane@MacBook-Air.local>
2020-09-18 00:31:05 -0400

    update

--- web/web/hub.php
+++ web/web/hub.php
@@ -175,7 +175,7 @@ $local_distance = array();
                   *  and not deleted
                   */
                   $result = pg_query($db, "SELECT DISTINCT organization.date, organization.time, organization.fiatvalue,organization.img, organization.id as org_key, organization.views,organization.description,organization.publickey, organization.address,organization.views, organization.url
-                  FROM organization AND date_submitted is not NULL AND date is not NULL AND date::timestamp >= NOW() ORDER BY organization.date, organization.views");
+                  FROM organization WHERE date_submitted is not NULL AND date is not NULL AND date::timestamp >= NOW() ORDER BY organization.date, organization.views");
 
  
                   if (pg_num_rows($result) > 0) {
@@ -190,7 +190,9 @@ $local_distance = array();
 
                     }
                   
-                  }else {array_push($errors_dashboard, "0 results");}
+                  }else {
+
+                  }
 
             pg_close($db);
 
