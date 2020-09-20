8b9f631e7468c5db3e52efcabd47a583ceef3611
Shane Michael Johnson <shane@MacBook-Air.local>
2020-09-20 11:51:24 -0400

    update

--- web/assets/js/dashboard.js
+++ web/assets/js/dashboard.js
@@ -3,7 +3,7 @@
 var dashboard_local_distance = <?php echo json_encode($dashboard_list, JSON_PRETTY_PRINT) ?>;
 var size = dashboard_local_distance.length; 
 var count = 0;
-console.log(dashboard_local_distance);
+//console.log(dashboard_local_distance);
 for (var i = dashboard_local_distance.length - 1; i >= 0; i--) {
  
    geolocation(dashboard_local_distance[i].address,dashboard_local_distance[i].publickey,size,count);
