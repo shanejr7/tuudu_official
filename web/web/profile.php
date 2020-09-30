b1814b61f8d48e165b3428b1e6376cef86ca67b7
Shane Michael Johnson <shane@MacBook-Air.local>
2020-09-30 08:43:54 -0400

    chat update

--- web/web/profile.php
+++ web/web/profile.php
@@ -1512,7 +1512,44 @@ console.log('yes');
    
    }
   })
+      $.ajax({
+   url:"fetch_user_profile_tab.php",
+   method:"POST",
+   data : {
+        publickey : publickey,
+        id : id 
+                    },
+   success:function(data){
+    $('#profile_tab_data').html(data);
 
+      $.ajax({
+   url:"fetch_user_connection_tab.php",
+   method:"POST",
+   data : {
+        publickey : publickey,
+        id : id 
+                    },
+   success:function(data){
+    $('#connection_follow_tab').html(data);
+
+      $.ajax({
+   url:"fetch_user_followers.php",
+   method:"POST",
+   data : {
+        publickey : publickey,
+        id : id 
+                    },
+   success:function(data){
+    $('#followers').html(data);
+     
+   }
+  })
+     
+   }
+  })
+     
+   }
+  })
 
  }
 
