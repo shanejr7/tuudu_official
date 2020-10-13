487c82e5ba27d0d168fd5bb13b3da7cbc1c1afb9
Shane Michael Johnson <shane@MacBook-Air.local>
2020-10-13 10:16:36 -0400

    update

--- web/web/order_page.php
+++ web/web/order_page.php
@@ -281,7 +281,7 @@ pg_close($conn);
 
                       echo '<div class="col-md-8"><h2 class="title"> ';
 
-                      echo strtoupper($ticket_name[0]).' ORDER</h2><h9>'.$order_list[0]["title"].'</h9>';
+                      echo strtoupper($order_list[0]["title"]).'\'S PAGE</h2>';
                       echo '</div>';
 
 
