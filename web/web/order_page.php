441c5dfeb3ae9033c0f9c3729cf7f4cfda34900c
Shane Michael Johnson <shane@MacBook-Air.local>
2020-10-15 10:19:09 -0400

    update messenger

--- web/web/order_page.php
+++ web/web/order_page.php
@@ -262,7 +262,7 @@ pg_close($conn);
       
         <div class="container">
        <div class="row"> 
-<!-- <div class="col-md-4"></div> -->
+<div class="col-md-4"></div>
       
 
           <?php   
