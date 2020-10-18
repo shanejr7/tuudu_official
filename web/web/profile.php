6c40798a8f69ab37ebb548e550f431ae263ae6a6
Shane Michael Johnson <shane@MacBook-Air.local>
2020-10-18 16:46:22 -0400

    update user prfile home post

--- web/web/profile.php
+++ web/web/profile.php
@@ -314,7 +314,7 @@ $bucket_name = 'tuudu-official-file-storage';
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit Picture</h5>
+    
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -329,10 +329,13 @@ $bucket_name = 'tuudu-official-file-storage';
 
 
         echo'<form enctype="multipart/form-data" method="post" action="user_image_upload.php">
-  <label>upload profile picture</label>
+ 
+                <div class="form-group label-floating has-success">
+                  <label class="control-label">EDIT AVATAR</label>
+                  </div>
                  <div class="row"> 
-                  <div class="col-md-4"></div>
-                  <div class="col-md-4">
+                  <div class="col-md-3"></div>
+                  <div class="col-md-6">
                    
                           <div class="form-group form-file-upload form-file-simple">
     <input type="text" class="form-control inputFileVisible" placeholder="upload image..." required>
@@ -357,7 +360,7 @@ $bucket_name = 'tuudu-official-file-storage';
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <!-- <h5 class="modal-title" id="exampleModalLabel">Add Post</h5> -->
+      
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
