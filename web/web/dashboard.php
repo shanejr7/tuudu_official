978743e5fb8eefb8bae7ed3c9df239178061dada
Shane Michael Johnson <shane@MacBook-Air.local>
2020-10-28 16:00:33 -0400

    update

--- web/web/dashboard.php
+++ web/web/dashboard.php
@@ -456,30 +456,30 @@ if (isset($temp) && $temp ==1) {
 
               $presignedUrl = (string)$request->getUri();
 
-              $uid = $item['org_id'];
+  //             $uid = $item['org_id'];
 
-   $db = pg_connect(getenv("DATABASE_URL"));
+  //  $db = pg_connect(getenv("DATABASE_URL"));
 
     
-    if (!$db) {
-       die("Connection failed: " . pg_connect_error());
-       header('location:oops.php');
-    }
+  //   if (!$db) {
+  //      die("Connection failed: " . pg_connect_error());
+  //      header('location:oops.php');
+  //   }
 
-  $result = pg_query($db, "SELECT DISTINCT profile_pic_src FROM users WHERE id =$uid");
-  $user = pg_fetch_assoc($result);
-  $uimg = $user['profile_pic_src'];
+  // $result = pg_query($db, "SELECT DISTINCT profile_pic_src FROM users WHERE id =$uid");
+  // $user = pg_fetch_assoc($result);
+  // $uimg = $user['profile_pic_src'];
 
-  pg_close($db);
+  // pg_close($db);
 
-          $cmd = $s3->getCommand('GetObject', [
-                                        'Bucket' => ''.$bucket_name.'',
-                                        'Key'    => ''.trim($uimg).'',
-                            ]);
+  //         $cmd = $s3->getCommand('GetObject', [
+  //                                       'Bucket' => ''.$bucket_name.'',
+  //                                       'Key'    => ''.trim($uimg).'',
+  //                           ]);
 
-              $request = $s3->createPresignedRequest($cmd, '+20 minutes');
+  //             $request = $s3->createPresignedRequest($cmd, '+20 minutes');
 
-              $presignedUrlUserPrf = (string)$request->getUri();
+  //             $presignedUrlUserPrf = (string)$request->getUri();
      
             
               echo '<div class="col-md-4">';
@@ -507,20 +507,20 @@ if (isset($temp) && $temp ==1) {
               
                 
 
-if($presignedUrlUserPrf && strlen(trim($uimg))>10 && ($fileChecker=='JPG' || $fileChecker=='JPEG' || $fileChecker=='PNG' || $fileChecker=='MOV')){
+// if($presignedUrlUserPrf && strlen(trim($uimg))>10 && ($fileChecker=='JPG' || $fileChecker=='JPEG' || $fileChecker=='PNG' || $fileChecker=='MOV')){
 
-                   echo '<div class="top-right col-md-2 col-sm-1 col-xs-1 h9"> 
-                        <a href="#"><img src="'.$presignedUrlUserPrf.'" class="img rounded" onload="myFunction('.$presignedUrlUserPrf.')"></a>
-                        </div>';
+//                    echo '<div class="top-right col-md-2 col-sm-1 col-xs-1 h9"> 
+//                         <a href="#"><img src="'.$presignedUrlUserPrf.'" class="img rounded" onload="myFunction('.$presignedUrlUserPrf.')"></a>
+//                         </div>';
 
                  
-              }else{
+//               }else{
 
-                 echo '<div class="top-right col-md-2 col-xs-1 h9"> 
-                        <a href="#"><img src="../assets/img/image_placeholder.jpg" class="img rounded"></a>
-                        </div>';
+//                  echo '<div class="top-right col-md-2 col-xs-1 h9"> 
+//                         <a href="#"><img src="../assets/img/image_placeholder.jpg" class="img rounded"></a>
+//                         </div>';
                  
-              } 
+//               } 
 
                    
 
