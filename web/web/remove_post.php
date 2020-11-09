5051daed3b6cfcf296ed444efe0dbd2a3d0d1355
Shane Michael Johnson <shane@MacBook-Air.local>
2020-11-09 12:11:09 -0500

    update: remove user posts

--- web/web/remove_post.php
+++ web/web/remove_post.php
@@ -6,22 +6,6 @@
   
 */
 
-// require('../aws/aws-autoloader.php');
-require('../aws/Aws/S3/S3Client.php'); 
-require('../aws/Aws/S3/ObjectUploader.php'); 
-
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-use Aws\S3\ObjectUploader;
-
-$s3=" ";
-$s3 = new Aws\S3\S3Client([
-    'version'  => 'latest',
-     'region'   => 'us-east-2',
-]);
-
-$bucket = getenv('S3_BUCKET')?: header('location:oops.php');
-$bucket_name = 'tuudu-official-file-storage';
 
 
 
