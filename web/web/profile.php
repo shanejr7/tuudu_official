3d3cf32c15d5dabc8926ecb6f0aa16d1ea99d790
Shane Michael Johnson <shane@MacBook-Air.local>
2020-11-10 09:55:12 -0500

    update: fav post ajax

--- web/web/profile.php
+++ web/web/profile.php
@@ -698,7 +698,7 @@ is not NULL ORDER BY organization.date");
 
 
                   echo '<div class="bottom-left" style="font-weight: bolder;">
-                        <a href="profile.php?publickey='.$item['publickey'].' class="fav_chat" data-key="'.$item['publickey'].'" data-id="'.$item['id'].'"">';
+                        <a href="#" class="fav_chat" data-key="'.$item['publickey'].'" data-id="'.$item['id'].'"">';
 
                         if ($item['favorite']==1) {
                           echo '<i class="material-icons" style="color:red;font-size:18pt;">favorite</i></a></div>';
