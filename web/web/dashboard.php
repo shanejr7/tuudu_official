3d568000ca6a10ccde09c71cfa4eb41db3a5af97
Shane Michael Johnson <shane@MacBook-Air.local>
2020-11-24 09:36:07 -0500

    update

--- web/web/dashboard.php
+++ web/web/dashboard.php
@@ -410,7 +410,7 @@ if (isset($temp) && $temp ==1) {
     <div class="profile-content " id="main">
       <div class="container">
 
-<div id="topics">
+<div class="scroll" id="topics">
         <nav class="nav">
 
 
@@ -423,6 +423,7 @@ if (isset($temp) && $temp ==1) {
   <a class="nav-link nav-item" href="#food" id="fd" role="tab" data-toggle="tab"><button type="button" class="btn btn-primary btn-round">Food</button></a>
   <a class="nav-link nav-item" href="#art" id="ar" role="tab" data-toggle="tab"><button type="button" class="btn btn-primary btn-round">Art</button></a>
   <a class="nav-link nav-item" href="#sports" id="sprt" role="tab" data-toggle="tab"><button type="button" class="btn btn-primary btn-round">Sports</button></a>
+    <a class="nav-link nav-item" href="#news" id="sprt" role="tab" data-toggle="tab"><button type="button" class="btn btn-primary btn-round">News</button></a>
   
 </nav>
 
